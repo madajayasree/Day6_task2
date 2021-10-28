c63b72c42719265ca752fab1a1e438fbffb9a61a
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-10-28 21:34:30 +0530

    Add files via upload

---
+++ stringbuilder/stringbuilder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder details;
+            details = new StringBuilder();
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+                        
+            Console.WriteLine("Enter age");
+            string age = Console.ReadLine();
+            
+            Console.WriteLine("Enter job");
+            string job = Console.ReadLine();
+
+            Console.WriteLine("Enter exp");
+            string exp = Console.ReadLine();
+
+            Console.WriteLine("Enter qualification");
+            string qualification = Console.ReadLine();
+
+            Console.WriteLine("name {0} , age{1} , job {2} , exp{3} and qualification{4} ", name, age, job , exp , qualification);
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
